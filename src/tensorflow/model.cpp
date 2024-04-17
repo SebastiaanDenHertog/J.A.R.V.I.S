d2956b4a40581beff583b6c376d938f2063e1f77
Sebastiaan <sebastiaan.denhertog@gmail.com>
2024-04-17 21:27:16 +0200

    Add model.h and model.cpp files for TensorFlow model implementation

---
+++ src/tensorflow/model.cpp
@@ -0,0 +1,21 @@
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/tools/gen_op_registration.h"
+
+class model
+{
+private:
+    /* data */
+public:
+    model(/* args */);
+    ~model();
+};
+
+model::model(/* args */)
+{
+}
+
+model::~model()
+{
+}
