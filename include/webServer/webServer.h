64ac346ce536e9a117bb7769c577ea8ad97f381b
Sebastiaan <sebastiaan.denhertog@gmail.com>
2024-09-21 13:49:32 +0200

    Refactor webServer.h to remove unnecessary code

--- include/webServer/webServer.h
+++ include/webServer/webServer.h
@@ -3,7 +3,6 @@
 
 #include <string>
 
-// Declaration of the setup_server function
 void setup_server(bool secure, const std::string &cert, const std::string &key, uint16_t port, int threads);
 
 #endif // WEB_SERVER_H
